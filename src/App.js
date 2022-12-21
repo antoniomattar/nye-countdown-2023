2b29dc758bae8a3cea2b2a94342b5af9e9edf427
antoniomattar <antoniomattar123@icloud.com>
2022-12-21 15:38:32 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react';
+import './App.scss';
+
+const NEW_YEARS_EVE_DATE = new Date(2022, 11, 32, 0, 0, 0)
+
+const getTimeDifference = () => {
+      const currentTime = new Date()
+      const difference = NEW_YEARS_EVE_DATE.getTime() - currentTime.getTime()
+
+      const days_diff = Math.floor(difference / (1000 * 60 * 60 * 24))
+      const hours_diff = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes_diff = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds_diff = Math.floor((difference % (1000 * 60)) / 1000)
+
+      return {
+        days_diff,
+        hours_diff,
+        minutes_diff,
+        seconds_diff
+      }
+}
+
 
 function App() {
-  return (
+  const [countdown,setCountdown] = useState();
+  const [loading,setLoading] = useState(true);
+  const [isNewYear,setIsNewYear] = useState(false)
+
+
+  useEffect(
+    () => {
+      const interval = setInterval(() => {
+
+        setLoading(false)
+        setCountdown(getTimeDifference())
+
+        if (countdown.days_diff <= 0
+          && countdown.hours_diff <= 0
+          && countdown.minutes_diff <= 0
+          && countdown.seconds_diff <= 0
+          ){
+          setIsNewYear(true)
+        }
+      }, 1000)
+      return () => clearInterval(interval)
+    }
+  )
+
+  if (loading) {
+    return (
+      <div className='container'>
+        <span className='font-big'>Loading...</span>
+      </div>
+    )
+  }
+
+  if (isNewYear){
+    return (
+      <div className='container'>
+        <span className='font-big'>Happy New Year!</span>
+      </div>
+    )
+  }
+
+
+  return (    
     <div className='container'>
 
-      <div>
-        <span>COMING </span>
-        <span>SOON...🔜</span>
+      <div className='flex'>
+        <span className='font-big'>{countdown.days_diff} </span>
+        <span className='font-small'>DAYS</span>
+      </div>
+
+
+      <div className='flex'>
+        <span className='font-big'>{countdown.hours_diff} </span>
+        <span className='font-small'>HOURS</span>
+      </div>
+
+
+      <div className='flex'>
+        <span className='font-big'>{countdown.minutes_diff} </span>
+        <span className='font-small'>MINUTES</span>
+      </div>
+
+
+      <div className='flex'>
+        <span className='font-big'>{countdown.seconds_diff} </span>
+        <span className='font-small'>SECONDS</span>
       </div>
 
     </div>
